a829eabd950a14b916e38269fd69c63816905e90
Sohel Rana <soheltech4@gmail.com>
2024-06-29 02:41:18 +0600

    start new redux

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default App;
