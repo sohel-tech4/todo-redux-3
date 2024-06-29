6c4cbf0ef5dbd2abda2f153aa61d92755ce7c5bd
Sohel Rana <soheltech4@gmail.com>
2024-06-30 01:40:41 +0600

    Make table

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import TodoCard from "./TodoCard";
+import { Button } from "../ui/button";
 
-const todoContainer = () => {
-    return (
-        <div>
-            <h1>Container</h1>
+const TodoContainer = () => {
+  return (
+    <div>
+      <div className="flex justify-between mb-5">
+        <Button className="bg-primary-gradient text-xl font-semibold">
+          Add Todo
+        </Button>
+        <Button className="bg-primary-gradient text-xl font-semibold">
+          Filter
+        </Button>
+      </div>
+      <div className="bg-primary-gradient p-[5px] w-full p-5 h-full rounded-md">
+        <div className="bg-white p-5 w-full space-y-3 rounded-lg h-full first-line:">
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
         </div>
-    );
+        {/* <div className="bg-white p-5 flex justify-center rounded-md font-semibold items-center">
+          <p>There is no task pending</p>
+        </div> */}
+      </div>
+    </div>
+  );
 };
 
-export default todoContainer;
+export default TodoContainer;
--- src/pages/todo/Todo.tsx
+++ src/pages/todo/Todo.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-
+import TodoContainer from "@/components/todo/TodoContainer";
+import Container from "@/components/ui/Container";
+import React from "react";
 
 const Todo = () => {
-    return (
-        <div>
-            <h1>Todo Page</h1>
-        </div>
-    );
+  return (
+    <Container>
+      <h1 className="text-center text-3xl my-10 font-semibold">My Todos</h1>
+      <TodoContainer></TodoContainer>
+    </Container>
+  );
 };
 
 export default Todo;
